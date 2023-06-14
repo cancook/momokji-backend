f2893af30fd83fcdf2011c0eb505c595599306d4
2044smile <2044smile@naver.com>
2023-06-14 13:32:25 +0000

    feat: serving static content by nginx

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+fblnkdhp78#eh0(i9r*y65b$5+qyn2hx7u-nzoivcb3770a0u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/ubuntu/code/self-dining-backend/'
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/youtube/', include('youtube.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
