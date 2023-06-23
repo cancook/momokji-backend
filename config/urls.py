ec04505d80d3c0af68b300d0bf71e679c69effb4
2044smile <2044smile@naver.com>
2023-06-23 23:21:15 +0900

    :bug: fix: minor

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from config import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/youtube/', include('youtube.urls')),
