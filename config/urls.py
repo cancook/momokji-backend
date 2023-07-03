2bdbd497df8ab6f57a0fe554102fa2a3218abd9f
2044smile <2044smile@naver.com>
2023-07-04 00:42:09 +0900

    :bug: fix: finally, Finding an error was successful

--- config/urls.py
+++ config/urls.py
@@ -27,7 +27,10 @@ urlpatterns = [
     path('api/', include('youtube.urls')),
     path('api/', include('search.urls')),
 
-    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+        # path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+ static(base.STATIC_URL, document_root=base.STATIC_ROOT)
