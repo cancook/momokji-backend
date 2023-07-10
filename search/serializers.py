776b96c8e925828584ebb8dda7d132055d40a62f
2044smile <2044smile@naver.com>
2023-07-10 17:40:06 +0900

    :recycle: refactor: add WordValidation ListField and CharField  and IngredientYouTubeSerializer
    IngredientYouTubeSerializer 에서 Response 를 변경하고 싶다. Ingredients_id 값이 dict 밖으로 나오게끔 왜냐하면 같은 값이 너무 반복되서 나옴

--- search/serializers.py
+++ search/serializers.py
@@ -1,11 +1,39 @@
 from rest_framework import serializers
-from .models import Ingredients
+
+from .models import Ingredients, Ingredients_Youtube
+from youtube.models import YouTube
 
 class WordValidationSerializer(serializers.Serializer):
-    word = serializers.CharField(help_text="단어로 검색")
+    word = serializers.ListField(
+        child=serializers.CharField(),
+        help_text="사용자가 입력한 단어로 유튜브 url_pk 검색"
+    )
 
 class IngredientSerializer(serializers.ModelSerializer):
-
+    
     class Meta:
         model = Ingredients
         fields = ['name']
+
+
+class IngredientYouTubeSerializer(serializers.ModelSerializer):
+    link = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Ingredients_Youtube
+        fields = ['ingredients_id', 'youtube_id', 'link']
+
+    def get_link(self, obj):
+        url_pk = YouTube.objects.get(id=obj.youtube_id).url_pk
+        return 'https://www.youtube.com/watch?v=' + url_pk
+    
+    # def to_representation(self, instance):
+    #     data = super(IngredientYouTubeSerializer, self).to_representation(instance)
+
+    #     response_dict = dict()
+    #     response_dict = {
+    #         'ingredients_id': instance.ingredients_id,
+    #         'youtube_id': instance.youtube_id,
+    #         'link': data['link']
+    #     }
+    #     return response_dict
