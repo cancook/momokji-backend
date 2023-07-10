94142e9844c53262cba23a30e432cfd8a24b44e0
2044smile <2044smile@naver.com>
2023-07-10 17:36:23 +0900

    :recycle: refactor: add ingredient-youtube-list

--- search/urls.py
+++ search/urls.py
@@ -6,6 +6,7 @@ app_name = 'search'
 
 router = DefaultRouter()
 router.register('ingredient-list', views.IngredientViewSet, basename='ingredient')
+router.register('ingredient-youtube-list', views.IngredientYouTubeViewSet, basename='ingredient-youtube')
 
 urlpatterns = [
     path('search/', include(router.urls)),
