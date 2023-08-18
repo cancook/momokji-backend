bfa992d5879d6c6905b7d3495164d15ae7e5feab
2044smile <2044smile@naver.com>
2023-08-19 00:22:55 +0900

    :recycle: Refactor: add __str__

--- search/models.py
+++ search/models.py
@@ -8,6 +8,9 @@ class CategoryIngredients(models.Model):
     class Meta:
         db_table = "search_category_ingredients"
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Ingredients(models.Model):
     name = models.CharField(unique=True, max_length=255)
@@ -15,6 +18,7 @@ class Ingredients(models.Model):
     category = models.ForeignKey(CategoryIngredients, related_name="category_ingredients", null=True, default=None, on_delete=models.SET_NULL)
     youtube = models.ManyToManyField(YouTube, related_name="ingredients", through="Ingredients_Youtube")
     is_valid = models.BooleanField(default=False)
+    is_not_connected = models.BooleanField(default=False)
 
     class Meta:
         db_table = "search_ingredients"
